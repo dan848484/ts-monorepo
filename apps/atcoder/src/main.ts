d23a37eea66423052fa660d01ae28ec1c3530ac7
fujisaki <fujidan8464@gmail.com>
2023-10-27 12:22:10 +0900

    nx run atcoder:watchを使って自動で再起動して標準入力を読める状態にできました

--- apps/atcoder/src/main.ts
+++ apps/atcoder/src/main.ts
@@ -1,4 +1,13 @@
-import * as fs from 'fs';
+// import * as fs from 'fs';
+// import { exec } from 'child_process';
+// import { argv } from 'process';
 
-const input = fs.readFileSync('/dev/stdin').toString();
-console.log(input);
+// const input = fs.readFileSync('/dev/stdin').toString();
+// const file = 'src/' + argv[2] + '/main.ts';
+// exec(`echo '${input}' | pnpm nodemon ${file}`, (error, stdout, stdin) => {
+//   if (error instanceof Error) {
+//     console.error(error);
+//   } else {
+//     console.log(stdout);
+//   }
+// });
