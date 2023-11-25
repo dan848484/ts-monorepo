3f44dc48a3ed5a8d23839023ad2a1d1a3291f510
fujisaki <fujidan8464@gmail.com>
2023-11-25 18:09:21 +0900

    思ったことのコメントを追加

--- apps/atcoder/src/main.ts
+++ apps/atcoder/src/main.ts
@@ -1,13 +1,43 @@
 // import * as fs from 'fs';
-// import { exec } from 'child_process';
-// import { argv } from 'process';
+// import { spawn } from 'child_process';
+// import { join } from 'path';
 
-// const input = fs.readFileSync('/dev/stdin').toString();
-// const file = 'src/' + argv[2] + '/main.ts';
-// exec(`echo '${input}' | pnpm nodemon ${file}`, (error, stdout, stdin) => {
-//   if (error instanceof Error) {
-//     console.error(error);
-//   } else {
-//     console.log(stdout);
-//   }
-// });
+// const input = fs.readFileSync(join(process.cwd(), 'stdin.txt')).toString();
+// const targetFilePath = join(process.cwd(), process.argv[2]);
+// console.log('start watching...: ', targetFilePath);
+// let lock = false;
+// let lastExecutionTime = 0;
+// const executeScript = () => {
+//   const currentTime = new Date().getTime();
+//   //executeScriptがなぜか2回呼び出されてしまうため、対策
+//   if (currentTime - lastExecutionTime < 2000 || lock) return;
+//   lock = true;
+//   console.log('-------------------');
+//   const proc = spawn(
+//     `echo "${input}" | ts-node -P ./tsconfig.base.json --esm ${targetFilePath} `,
+//     [],
+//     {
+//       shell: true,
+//     }
+//   );
+//   proc.stdout.on('data', (data) => {
+//     console.log(data.toString());
+//   });
+//   proc.stderr.on('data', (data) => {
+//     console.error(data.toString());
+//   });
+//   proc.on('close', () => {
+//     lock = false;
+//     lastExecutionTime = new Date().getTime();
+//   });
+// };
+// executeScript();
+// fs.watch(targetFilePath, executeScript);
+
+/**
+ * atcoder app 自体に変更検知機能を搭載したいと思ったが、、、、
+ *
+ * そもそも app として atcoderを使うことが思想として異なる気がする。
+ *
+ * あくまでも開発時のツールだから app 自体の機能として実装するのはうまくいかないはず。
+ */
