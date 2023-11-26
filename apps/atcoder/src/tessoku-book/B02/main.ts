9d93e9f2f0a618e9f5ab82ee3bf5edf6b5c37256
fujisaki <fujidan8464@gmail.com>
2023-11-27 02:23:08 +0900

    tessoku B02

---
+++ apps/atcoder/src/tessoku-book/B02/main.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+/**
+ * A 以上 B 以下の整数のうち、100 の約数であるものは存在しますか。
+ * @param input
+ */
+export const main = (input: string) => {
+  const lines = input.split('\n');
+  const [A, B] = lines[0].split(' ').map(Number);
+  for (let i = A; i <= B; i++) {
+    if (100 % i === 0) {
+      console.log('Yes', i);
+      return;
+    }
+  }
+  console.log('No');
+};
+
+main(fs.readFileSync('/dev/stdin').toString());
