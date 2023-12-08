9e827a9bf59bbb2b6812c31184ce8294705666e6
fujisaki <fujidan8464@gmail.com>
2023-12-08 15:00:05 +0900

    docs: ✏️ コメントを追加

--- apps/atcoder/src/tessoku-book/A18/main.ts
+++ apps/atcoder/src/tessoku-book/A18/main.ts
@@ -1,4 +1,20 @@
 import * as fs from 'fs';
+/**
+ * https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_r
+ * A18 Subset Sum
+ *
+ * 解法メモ
+ *
+ * dpは2次元配列。行は0~N行あって、何番目のカードまで使って考えるかを表す。
+ * 列は0~S列あって、合計を表す。(p116参照)
+ *
+ * 行→i、列→j
+ * ・カードi-1の時点で合計がすでにjであればカードiを選ぶ必要はないから、マルをつける。
+ * ・カードi-1の時点で合計がj-Aiであればカードiを選ぶとちょうど合計がjとなるのでマルをつける。
+ *
+ *
+ * @param input
+ */
 const main = (input: string) => {
   const lines = input.split('\n');
   const [N, S] = lines[0].split(' ').map(Number);
