0b3d318239f969a67b13e4aac1a8b819fb31e6e2
fujisaki <fujidan8464@gmail.com>
2023-12-08 14:49:13 +0900

    fix: 🐛 動的計画法を用いた実装

--- apps/atcoder/src/tessoku-book/A18/main.ts
+++ apps/atcoder/src/tessoku-book/A18/main.ts
@@ -3,20 +3,19 @@ const main = (input: string) => {
   const lines = input.split('\n');
   const [N, S] = lines[0].split(' ').map(Number);
   const A = lines[1].split(' ').map(Number);
-  if (S === 0) {
-    console.log('Yes');
+  const dp = new Array(N + 1);
+  for (let i = 0; i < dp.length; i++) {
+    dp[i] = new Array(S + 1).fill(false);
   }
-  for (let i = 0; i < A.length; i++) {
-    let subsetSum = 0;
-
-    for (let j = i; j < A.length; j++) {
-      subsetSum += A[j];
-      if (subsetSum === S) {
-        console.log('Yes');
-        return;
+  dp[0][0] = true;
+  for (let i = 1; i < dp.length; i++) {
+    const Ai = A[i - 1];
+    for (let j = 0; j < dp[0].length; j++) {
+      if (dp[i - 1][j] || (j >= Ai && dp[i - 1][j - Ai])) {
+        dp[i][j] = true;
       }
     }
   }
-  console.log('No');
+  console.log(dp[N][S] ? 'Yes' : 'No');
 };
 main(fs.readFileSync('/dev/stdin').toString());
