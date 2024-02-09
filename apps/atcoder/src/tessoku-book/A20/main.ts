fcd78fd7c06954a09b4212d4786470bc6af07a9c
fujisaki <fujidan8464@gmail.com>
2024-02-09 16:00:07 +0900

    A20 LCS

--- apps/atcoder/src/tessoku-book/A20/main.ts
+++ apps/atcoder/src/tessoku-book/A20/main.ts
@@ -18,8 +18,19 @@ const main = (input: string) => {
   const dp = new Array(rows)
     .fill(undefined)
     .map((_) => new Array(columns).fill(0));
-  dp[0][0] = 0;
-
-  console.log(dp);
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      if (i >= 1 && j >= 1 && S[i - 1] === T[j - 1]) {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] + 1);
+      } else if (i >= 1 && j >= 1) {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      } else if (i >= 1) {
+        dp[i][j] = dp[i - 1][j];
+      } else if (j >= 1) {
+        dp[i][j] = dp[i][j - 1];
+      }
+    }
+  }
+  console.log(dp[rows - 1][columns - 1]);
 };
 main(fs.readFileSync('/dev/stdin').toString());
