5fa4e5c44dcae218478f25fa9e85fa17cda14fe7
fujisaki <fujidan8464@gmail.com>
2024-03-14 10:49:56 +0900

    selection sortの実装

--- libs/selection-sort/src/lib/selection-sort.ts
+++ libs/selection-sort/src/lib/selection-sort.ts
@@ -1,3 +1,15 @@
-export function selectionSort(): string {
-  return 'selection-sort';
-}
+export const selectionSort = (nums: number[]) => {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    let max = nums[i];
+    let maxIndex = i;
+    for (let j = 0; j < i; j++) {
+      if (nums[j] > max) {
+        max = nums[j];
+        maxIndex = j;
+      }
+    }
+    const cache = nums[i];
+    nums[i] = nums[maxIndex];
+    nums[maxIndex] = cache;
+  }
+};
